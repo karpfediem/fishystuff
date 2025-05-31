53e0f6c16e76102b532ee8f67d559112952566b1
Karpfen <karpfen@karpfen.dev>
2025-05-31 16:06:46 +0200

    bot: Make Crio poke-able

--- bot/src/main.rs
+++ bot/src/main.rs
@@ -1,18 +1,15 @@
+mod poke;
+
 use poise::serenity_prelude as serenity;
 
 struct Data {} // User data, which is stored and accessible in all command invocations
 type Error = Box<dyn std::error::Error + Send + Sync>;
 type Context<'a> = poise::Context<'a, Data, Error>;
 
-/// Displays your or another user's account creation date
+/// Poke Crio, see what happens
 #[poise::command(slash_command, prefix_command)]
-async fn age(
-    ctx: Context<'_>,
-    #[description = "Selected user"] user: Option<serenity::User>,
-) -> Result<(), Error> {
-    let u = user.as_ref().unwrap_or_else(|| ctx.author());
-    let response = format!("{}'s account was created at {}", u.name, u.created_at());
-    ctx.say(response).await?;
+async fn poke(ctx: Context<'_>) -> Result<(), Error> {
+    ctx.say(poke::respond()).await?;
     Ok(())
 }
 
@@ -20,7 +17,11 @@ async fn age(
 ///
 /// Running this function directly, without any subcommand, is only supported in prefix commands.
 /// Discord doesn't permit invoking the root command of a slash command if it has subcommands.
-#[poise::command(prefix_command, slash_command, subcommands("help_drr", "help_mystical"))]
+#[poise::command(
+    prefix_command,
+    slash_command,
+    subcommands("help_drr", "help_mystical")
+)]
 pub async fn help(ctx: Context<'_>) -> Result<(), Error> {
     ctx.say("What can I help you with? *qweek*").await?;
     Ok(())
@@ -44,12 +45,11 @@ async fn main() {
     tracing_subscriber::fmt::init();
 
     let token = std::env::var("DISCORD_TOKEN").expect("missing DISCORD_TOKEN");
-    let intents =
-        serenity::GatewayIntents::non_privileged();
+    let intents = serenity::GatewayIntents::non_privileged();
 
     let framework = poise::Framework::builder()
         .options(poise::FrameworkOptions {
-            commands: vec![age(), help()],
+            commands: vec![poke(), help()],
             ..Default::default()
         })
         .setup(|ctx, _ready, framework| {
---
+++ bot/src/poke/mod.rs
@@ -0,0 +1,57 @@
+use rand::seq::SliceRandom;
+use rand::thread_rng;
+
+const PHRASES: &[&str] = &[
+    "Eep! Don’t poke the Crio, silly land-walker!",
+    "Poke me again and I’ll splash ya, squeak!",
+    "You got some nerve, aye? I *like* it!",
+    "Is this how you greet fine otters? Rude!",
+    "Oof! That tickled my whiskers, squeak!",
+    "Hands off the fur, it's maintenance day!",
+    "Oi! I'm workin' here, or at least pretendin'!",
+    "That’s Crio harassment, that is!",
+    "You'll get fined for pokin’ Crio, y’know?",
+    "Crio not happy. Crio considering revolution.",
+    "Touch me again and I’ll turn you into fish bait!",
+    "Poke me one more time and I’ll make you chum!",
+    "A sea otter’s patience is deep… but not endless!",
+    "I’ve wrestled sea monsters nicer than you!",
+    "Even the waves got more manners!",
+    "Don’t make me slap you with a mackerel!",
+    "You wanna go fish-wrangling with yer nose?",
+    "Careful, landlubber. I bite *and* splash.",
+    "Fishin’ for trouble, eh?",
+    "I’ll tell the tides on you!",
+    "Crio is a very important otter. Show respect!",
+    "I am *Crio the Unpoked*! Or I *was*...",
+    "Do you *know* who I am?! No? Fair.",
+    "Pokin’ Crio is a declaration of war!",
+    "I’ll report you to the Otter Council!",
+    "This is why I prefer seagulls… mostly.",
+    "Bananafish pudding! Wait, what?",
+    "If I explode, it’s your fault.",
+    "Crio feels a great disturbance… in his fur.",
+    "Do pokes count as blessings? I forget!",
+    "You’ve awakened the forbidden itch!",
+    "I smell... treachery. Or herring.",
+    "Stop or I’ll summon the ancient barnacle!",
+    "I once saw a starfish do a dance. True story!",
+    "You poke Crio, Crio poke destiny!",
+    "Three pokes and I turn into a tuna. Probably.",
+    "How bored *are* you, adventurer?",
+    "There’s a whole war goin’ on and *this* is your priority?",
+    "Bet you poke all the NPCs. Freak.",
+    "Is this your idea of endgame content?",
+    "Why don't you poke some *rocks*, huh?",
+    "You should be ashamed. But I'm kinda impressed.",
+    "Ten pokes unlocks a secret fish! (Or was it 15...?)",
+    "Poking NPCs: the *true* Black Desert experience.",
+    "You better not be streaming this!",
+    "Crio will remember this.",
+];
+
+pub fn respond() -> String {
+    let mut rng = thread_rng();
+    let x = PHRASES.choose(&mut rng).unwrap_or(&"Qweek!");
+    x.to_string()
+}
