7517a9658f0ace37ebc7dc6c76229f5da98b8b8e
Karpfen <karpfen@karpfen.dev>
2025-06-01 20:41:36 +0200

    feat(bot): Add help topics

--- bot/src/help/mod.rs
+++ bot/src/help/mod.rs
@@ -1,6 +1,7 @@
 use crate::utils::fuzzy::gen_autocomplete;
 use crate::{Context, Error};
 use futures::{stream, Stream};
+use poise::serenity_prelude::Builder;
 use std::str::FromStr;
 use strum::{Display, EnumIter, EnumString, IntoEnumIterator};
 
@@ -12,8 +13,14 @@ enum HelpTopic {
     DRR,
     #[strum(serialize = "Experience")]
     Experience,
+    #[strum(serialize = "Gear")]
+    Gear,
+    #[strum(serialize = "Map")]
+    Map,
     #[strum(serialize = "Money")]
     Money,
+    #[strum(serialize = "Waypoints")]
+    Waypoints,
 }
 
 async fn autocomplete_fuzzy_help<'a>(
@@ -35,7 +42,12 @@ pub async fn help(
     match HelpTopic::from_str(topic.as_str()) {
         Ok(topic) => match topic {
             HelpTopic::Mystical => help_mystical(ctx).await,
-            _ => help_reject(ctx).await,
+            HelpTopic::DRR => help_drr(ctx).await,
+            HelpTopic::Experience => help_experience(ctx).await,
+            HelpTopic::Gear => help_gear(ctx).await,
+            HelpTopic::Map => help_map(ctx).await,
+            HelpTopic::Waypoints => help_waypoints(ctx).await,
+            HelpTopic::Money => help_money(ctx).await,
         },
         Err(_e) => help_reject(ctx).await,
     }?;
@@ -47,8 +59,59 @@ async fn help_reject(ctx: Context<'_>) -> Result<(), Error> {
         .await?;
     Ok(())
 }
+
 async fn help_mystical(ctx: Context<'_>) -> Result<(), Error> {
-    ctx.send(poise::CreateReply::default().content("Mystical Fish! :)"))
-        .await?;
+    ctx.send(poise::CreateReply::default().content(
+        "https://discord.com/channels/161861855332139008/1377294298869137530/1377294298869137530",
+    ))
+    .await?;
+    Ok(())
+}
+
+async fn help_drr(ctx: Context<'_>) -> Result<(), Error> {
+    ctx.send(poise::CreateReply::default().content(
+        "https://discord.com/channels/161861855332139008/1378355406966886421/1378355406966886421",
+    ))
+    .await?;
+    Ok(())
+}
+
+async fn help_experience(ctx: Context<'_>) -> Result<(), Error> {
+    ctx.send(poise::CreateReply::default().content(
+        "https://discord.com/channels/161861855332139008/1377294657234669598/1377294657234669598",
+    ))
+    .await?;
+    Ok(())
+}
+
+async fn help_gear(ctx: Context<'_>) -> Result<(), Error> {
+    ctx.send(poise::CreateReply::default().content(
+        "https://discord.com/channels/161861855332139008/1378397306394644490/1378397306394644490\n\
+        https://discord.com/channels/161861855332139008/1378333117072408628/1378333117072408628",
+    ))
+    .await?;
+    Ok(())
+}
+async fn help_map(ctx: Context<'_>) -> Result<(), Error> {
+    ctx.send(poise::CreateReply::default().content(
+        "https://discord.com/channels/161861855332139008/1377401205088583690/1377401205088583690",
+    ))
+    .await?;
+    Ok(())
+}
+
+async fn help_waypoints(ctx: Context<'_>) -> Result<(), Error> {
+    ctx.send(poise::CreateReply::default().content(
+        "https://discord.com/channels/161861855332139008/1377387923326107819/1377387923326107819",
+    ))
+    .await?;
+    Ok(())
+}
+
+async fn help_money(ctx: Context<'_>) -> Result<(), Error> {
+    ctx.send(poise::CreateReply::default().content(
+        "https://discord.com/channels/161861855332139008/1378361389030178836/1378361389030178836",
+    ))
+    .await?;
     Ok(())
 }
