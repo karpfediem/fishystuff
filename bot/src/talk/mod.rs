7c8e7a27c772093c988d97cb665cebc236f155d9
Karpfen <karpfen@karpfen.dev>
2025-07-02 23:20:28 +0200

    fix(bot): Restrict usage of talk command
    Limit to MaoMao's discord server id

--- bot/src/talk/mod.rs
+++ bot/src/talk/mod.rs
@@ -8,7 +8,14 @@ pub async fn talk(
     ctx: Context<'_>,
     #[description = "What's on Crio's mind?"] wisdom: String,
 ) -> Result<(), Error> {
-    let create_message = CreateMessage::new().content(wisdom);
+    let mut message_content = String::from("Qweek!");
+    if let Some(guild_id) = ctx.guild_id() {
+        if guild_id == 161861855332139008 {
+            message_content = wisdom;
+        }
+    }
+
+    let create_message = CreateMessage::new().content(message_content);
     ctx.channel_id().send_message(ctx, create_message).await?;
     ctx.send(CreateReply::default().ephemeral(true).content("Qweek."))
         .await?;
